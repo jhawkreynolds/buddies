675ce39c232667e21a1dee709dd8163137c5b7d2
jhawkreynolds <112728229+jhawkreynolds@users.noreply.github.com>
2023-07-11 16:29:07 -0500

    Update verification.php

--- pages/verification/verification.php
+++ pages/verification/verification.php
@@ -42,9 +42,11 @@ try{
                 "hash" => $hash
             );
             $json_string = json_encode($json_data, JSON_PRETTY_PRINT);
+            /*
             $file = fopen($filepath, 'w');
             fwrite($file, $json_string);
             fclose($file);
+            */
             break;
         case 'login':
             // input validation
