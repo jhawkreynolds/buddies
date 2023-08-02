539e2446ff8728b7862ec3ea1651e0bfffb6a588
jhawkreynolds <112728229+jhawkreynolds@users.noreply.github.com>
2023-08-02 18:43:05 -0500

    Update pageUI.js

--- pages/js/pageUI.js
+++ pages/js/pageUI.js
@@ -202,7 +202,8 @@ function load_doc(url, cFunction, hash) {
     xhttp=new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        return cFunction(this, hash);
+        return false;
+        //return cFunction(this, hash);
       }
     };
     xhttp.open("GET", url, true);
